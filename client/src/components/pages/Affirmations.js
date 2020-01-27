469510f04db5ab2e94addf9932cef0091cc9f3df
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-01-27 09:40:34 -0800

    Update Affirmations.js
    removed affirmationContainer until problem with State is figured out

--- client/src/components/pages/Affirmations.js
+++ client/src/components/pages/Affirmations.js
@@ -1,15 +1,12 @@
 import React, { Fragment } from "react";
 import AffirmationForm from "../affirmations/affirmationForm";
 // import AffirmationItem from "../affirmations/affirmationItem";
-import AffirmationContainer from "../affirmations/affirmationContainer";
+// import AffirmationContainer from "../affirmations/affirmationContainer";
 
 const Affirmations = () => {
   return (
-    // This will have a carousel that takes affirmations from MongoDB and flashes them across the screen in set intervals.
-    // Will have functionality to see affirmations all at once, and add them, subtract them, etc.
-
     <Fragment>
-      <AffirmationContainer />
+      {/* <AffirmationContainer /> */}
       <AffirmationForm />
       {/* <AffirmationItem /> */}
     </Fragment>
